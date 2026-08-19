34c58d5289d512905ac83a1bd1d5ca4264a0d7ac
AlexBanks106 <picassobanks@gmail.com>
2026-08-19 11:05:11 -0700

    Update practice.js

--- lesson3/classwork/practice.js
+++ lesson3/classwork/practice.js
@@ -11,27 +11,18 @@
 
 
 // Problem 3
-// Create a variable for a number.
-// Print "Fizz" if it is divisible by 3, "Buzz" if divisible by 5,
-// print "FizzBuzz" if divisible by both 3 and 5,
-// otherwise print the number itself.
-
+// 87
+console.log "Fizz"
 
 
 // Problem 4
-// Create a variable for a password.
-// If the password is "sova" or "coding", print "Access granted".
-// Otherwise print "Access denied".
+    console.log
+console.log "Access granted
+
 
 
 
 // Problem 5
-// Create a variable for a test score (0-100).
-// Print the grade based on score:
+// 100
+console.log "A"
 //   90 and above: "A"
-//   80 to 89: "B"
-//   70 to 79: "C"
-//   60 to 69: "D"
-//   below 60: "F"
-// Use nested if or else if statements.
-
