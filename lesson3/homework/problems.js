1c75ae3a609d751bfe40377b2ec433d532a0f059
AlexBanks106 <picassobanks@gmail.com>
2026-08-19 11:33:54 -0700

    Update problems.js

--- lesson3/homework/problems.js
+++ lesson3/homework/problems.js
@@ -2,29 +2,33 @@
 // Create a variable for a number.
 // Print whether the number is positive, negative, or zero.
 
-
+45
+console.log positive
 
 // Problem 2
 // Create a variable for a number.
 // Print whether it is even or odd.
 
-
-
+44 
+console.log odd
 
 // Problem 3
 // Create variables for age and has_permission.
 // Print "Can go" if age is at least 12 or has_permission is true.
 // Otherwise print "Cannot go".
 
-
+67 can go
+69 can't go
 
 // Problem 4
 // Create a variable for a number.
 // Print whether it is divisible by 2 and 3.
 
-
+12
+2\3
 
 // Problem 5
 // Create a variable for a temperature.
 // Print "Cold" if below 50, "Warm" if 50-79, and "Hot" if 80 or above.
-
+1000000
+very warm
