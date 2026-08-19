29d17bde8cea4ccb4f2e4a34b612f28f7076e89c
AlexBanks106 <picassobanks@gmail.com>
2026-08-19 10:40:06 -0700

    Update if_else.js

--- lesson3/classwork/if_else.js
+++ lesson3/classwork/if_else.js
@@ -1,4 +1,27 @@
 // if statement: runs only when the condition is true
 
 let age = 17;
-if (age 
+if (age>= 18) {
+    console.log("You can vote!");
+}
+let temp = 12;
+if (temp < 10) {
+    console.log("It's cold wear a jacket!");
+} else {
+    console.log("No jacket needed!");
+}
+
+// if/else if/else: handle multiple specific cases 
+let score = 94;
+
+if (score >= 90) {
+    console.log("A");
+} else if (score >= 80) {
+    console.log("B");
+} else if (score >= 70) {
+    console.log("C");
+} else if (score >= 60) {
+    console.log("D");
+    } else {
+        console.log("F");
+    }
